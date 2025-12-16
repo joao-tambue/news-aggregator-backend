cbc73bc86f5c7e32361ac014e764ba1eca6b25e1
João Tambue <tjoao7279@gmail.com>
2025-12-16 19:02:41 +0100

    feat: add filter function

--- src/controllers/news.controller.ts
+++ src/controllers/news.controller.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from "express";
 import { fetchTopHeadlines } from "../services/news.service";
 
-export async function getTopHeadlines(
-  req: Request,
-  res: Response
-) {
+export async function getTopHeadlines(req: Request, res: Response) {
   try {
-    const category =
-      (req.query.category as string) || "All";
+    const { category, search, source, date } = req.query;
 
-    const data = await fetchTopHeadlines(category);
+    const data = await fetchTopHeadlines({
+      category: category as string,
+      search: search as string,
+      source: source as string,
+      date: date as string,
+    });
 
     res.status(200).json(data);
   } catch (error) {
