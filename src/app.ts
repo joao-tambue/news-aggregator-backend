e31e282c948c8f0238d6142cc4fae78094f9a321
João Tambue <tjoao7279@gmail.com>
2025-12-16 17:48:12 +0100

    fix: cors add

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import newsRoutes from "./routes/news.routes";
 import { apiRateLimit } from "./middlewares/rateLimit";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(apiRateLimit);
+
+app.use(cors({
+    origin: [
+        "http://localhost:3000",
+        "https://news-aggregator-sigma-sooty.vercel.app/"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}))
 
 app.use("/api", newsRoutes);
 
