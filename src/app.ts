f3fecffbcd927cc13b82545d2a52723dd58370e7
João Tambue <tjoao7279@gmail.com>
2025-12-16 18:04:55 +0100

    feat: change the cors

--- src/app.ts
+++ src/app.ts
@@ -9,14 +9,7 @@ app.use(express.json());
 
 app.use(apiRateLimit);
 
-app.use(cors({
-    origin: [
-        "http://localhost:3000",
-        "https://news-aggregator-sigma-sooty.vercel.app/"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-}))
+app.use(cors());
 
 app.use("/api", newsRoutes);
 
